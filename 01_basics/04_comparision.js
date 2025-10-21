e82690a45680bac944d28681bc7e7411f7a983ea
kunalTaywade <taywadekunal32@gmail.com>
2025-10-21 19:17:01 +0000

    Compare

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1); 
+// console.log("02" > 1);
+//in here the js will treat the string as a number 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);//In here the equality check == and comparision check <>= works differently .Comparision converts null to number ,treating it as 0
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === => using this the comparision will now happens b/w data types
+
+console.log("2" === 2);
