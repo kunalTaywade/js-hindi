20547e5f6bbd0068902904e6ff29e17521785e6c
kunalTaywade <taywadekunal32@gmail.com>
2025-10-21 19:00:38 +0000

    Operation disscused

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,77 @@
+let score = true
+
+// console.log(score)
+// console.log(typeof(score))
+
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 , false => 0
+
+let isLoggedIn = undefined
+// console.log(typeof(isLoggedIn))
+
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof(booleanisLoggedIn))
+// console.log(booleanisLoggedIn)
+
+//1 => true , 0 => false
+//null || undefined => false
+//"" => false
+//"kunal" => true
+
+let someNumber = 33
+
+let stringsomeNumber = String(someNumber)
+// console.log(stringsomeNumber);
+// console.log(typeof stringsomeNumber);
+
+
+
+// *********************** Operations ***********************
+
+
+let value = 3
+
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hi"
+let str2 = " I am kunal"
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + "2")
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + 1 + "2")
+// console.log("1" + 1 + 2)
+
+// console.log( (3+4) * 5 % 3)
+
+// console.log(+true);
+// console.log(+"");
+// console.log(typeof(+""));
+
+let num1 ,num2 , num3
+num1 = num2 = num3 =2*2
+
+//Prefix and Postfix
+let gameCounter = 100
+++gameCounter
+// gameCounter++
+console.log(gameCounter);
+
