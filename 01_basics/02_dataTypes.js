4b53d93cf13d415e44bd339785a8ace6ee65f60c
kunalTaywade <taywadekunal32@gmail.com>
2025-10-21 17:35:28 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+console.log(3+
+    3
+) // code readability should be high
+
+
+console.log("kunal")
+
+let name = "kunal"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53
+//bignit
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+
+
+console.table([name,age,isLoggedIn,state])
+
+console.log(typeof undefined)
+console.log(typeof null) //object
+
